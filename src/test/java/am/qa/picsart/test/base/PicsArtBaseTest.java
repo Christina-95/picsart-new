5b44bf4bf7fb7cc8bdbdcb3b86a76b454d5a5db3
Christina-95 <christinasargsyan@gmail.com>
2020-03-23 18:02:43 +0400

    Changes in the PicsArtBaseTest

--- src/test/java/am/qa/picsart/test/base/PicsArtBaseTest.java
+++ src/test/java/am/qa/picsart/test/base/PicsArtBaseTest.java
@@ -19,39 +19,50 @@ import am.qa.picsart.pages.user.PicsArtUserPage;
 
 public class PicsArtBaseTest {
 	
-		protected static final String HOME_PAGE_URL = "https://picsart.com";
-		protected Properties properties;
-		protected WebDriver driver; 
+	protected static final String HOME_PAGE_URL = "https://picsart.com";
+	protected Properties properties;
+	protected WebDriver driver; 
 	
 		
-		@BeforeTest()
-		public void prepareEnv() throws IOException {
-			properties = new Properties();
-			properties.load(getClass().getClassLoader().getResourceAsStream("readProperty.properties"));
-			System.setProperty("webdriver.chrome.driver", properties.getProperty("chromeDriver"));
-			driver = new ChromeDriver();
-			driver.get(HOME_PAGE_URL);
-			//driver.manage().window().maximize();
-			PicsArtBasePage page = new PicsArtBasePage(driver);
-			WebDriverWait wait = new WebDriverWait(driver, 30);
-			wait.until(ExpectedConditions.visibilityOf(page.closePopUp));
-			page.moveToClosePopUp();
-			page.closePopUp.click();
-			wait.until(ExpectedConditions.visibilityOf(page.loginButton));
-			page.loginButton.click();
-	        PicsArtLoginPage loginPage = new PicsArtLoginPage(driver);
-	        wait.until(ExpectedConditions.visibilityOf(loginPage.usernameOrEmailField));
-	        loginPage.usernameOrEmailField.click();
-	        loginPage.usernameOrEmailField.sendKeys("tinat6043@gmail.com");
-	        wait.until(ExpectedConditions.visibilityOf(loginPage.passwordField));
-	        loginPage.passwordField.click();
-	        loginPage.passwordField.sendKeys("12345678");
-	        wait.until(ExpectedConditions.visibilityOf(loginPage.signInButton));
-			loginPage.signInButton.click();
-	        PicsArtUserPage userPage = new PicsArtUserPage(driver);
-	        userPage.validateLoggedInUser();
+	@BeforeTest()
+	public void prepareEnv() throws IOException {
+	properties = new Properties();
+	properties.load(getClass().getClassLoader().getResourceAsStream("readProperty.properties"));
+	System.setProperty("webdriver.chrome.driver", properties.getProperty("chromeDriver"));
+	driver = new ChromeDriver();
+	driver.get(HOME_PAGE_URL);
+	//driver.manage().window().maximize();
+	
+	
+	PicsArtBasePage page = new PicsArtBasePage(driver);
+	WebDriverWait wait = new WebDriverWait(driver, 30);
+	wait.until(ExpectedConditions.visibilityOf(page.closePopUp));
+	page.moveToClosePopUp();
+	//close the Pop-up
+	page.closePopUp.click();
+	
+	//click on the "Log In" button
+	wait.until(ExpectedConditions.visibilityOf(page.loginButton));
+	page.loginButton.click();
+	
+	//Fill in the username and password fields
+	PicsArtLoginPage loginPage = new PicsArtLoginPage(driver);
+	wait.until(ExpectedConditions.visibilityOf(loginPage.usernameOrEmailField));
+	loginPage.usernameOrEmailField.click();
+	loginPage.usernameOrEmailField.sendKeys("tinat6043@gmail.com");
+	wait.until(ExpectedConditions.visibilityOf(loginPage.passwordField));
+	loginPage.passwordField.click();
+	loginPage.passwordField.sendKeys("12345678");
+	
+	//click on the "Sign In " button
+	wait.until(ExpectedConditions.visibilityOf(loginPage.signInButton));
+	loginPage.signInButton.click();
+	
+	//validate that the user is logged in
+	PicsArtUserPage userPage = new PicsArtUserPage(driver);
+	userPage.validateLoggedInUser();
 			
-		}
+	}
 		
 		
 //		@AfterTest
